fffdc63eaad62a40e4ccb5575b12c6bde8b5a074
FixFips <fixfips1@hotmail.com>
2025-11-25 01:25:26 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import './App.css'
 import Search from './components/Search'
 import Spinner from './components/Spinner'
 import MovieCard from './components/MovieCard'
@@ -62,31 +61,31 @@ function App() {
 
   return (
     <main>
-      <div className='Pattern' />
+      <div className='pattern' />
 
-      <div className='wrapper' />
+      <div className='wrapper'>
 
-      <header>
-        <img src='./hero-img.png' alt="Hero Banner" />
-        <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle!</h1>
+        <header>
+          <img src='./hero-img.png' alt="Hero Banner" />
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle!</h1>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      </header>
-      <section className="all-movies">
-        <h2 className='mt-[50px]'>All movies</h2>
-        {isLoading ? (
-          <Spinner />
-        ) : errorMessage ? (
-          <p className="text-red-500">{errorMessage}</p>
-        ) : (
-          <ul>
-            {movieList.map((movie) => (
-              <MovieCard key={movie.id} movie={movie} />
-            ))}
-          </ul>
-        )}
-      </section>
-
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className="all-movies">
+          <h2 className='mt-[50px]'>All movies</h2>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
     </main>
 
   )
